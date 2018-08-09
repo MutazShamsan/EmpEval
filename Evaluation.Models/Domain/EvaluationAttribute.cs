0e38203cccae7ed72ffc68903d8cbaf400b2da8c
Mutaz Abdulkareem Mohammed Shamsan <mutaz.shamsan@gmail.com>
2018-08-09 18:15:00 +0800

    adding gulp

--- Evaluation.Models/Domain/EvaluationAttribute.cs
+++ Evaluation.Models/Domain/EvaluationAttribute.cs
@@ -7,5 +7,6 @@ namespace Evaluation.Models.Domain
     class EvaluationAttribute : BaseModel
     {
         public string Name { get; set; }
+        public string Description { get; set; }
     }
 }
--- Evaluation.Web/gulpfile.js
+++ Evaluation.Web/gulpfile.js
@@ -26,6 +26,7 @@ var vendorScripts = [
     "node_modules/jquery/dist/jquery.min.js",
     "node_modules/popper.js/dist/umd/popper.min.js",
     "node_modules/bootstrap/dist/js/bootstrap.min.js",
+    "node_modules/datatables.net/js/jquery.dataTables.min.js"
 ];
 
 gulp.task('default', ['build-vendor']);
